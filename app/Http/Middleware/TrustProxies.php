371f72e3859f67eabb38b87bca430ae91a069b17
Jean Marques <jean.magic@hotmail.com>
2025-04-07 21:08:57 -0300

    fix: adjust erros to upgrade version in kernel and middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,22 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string
-     */
-    protected $proxies;
+    protected $proxies = '*';
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected function getHeaders(Request $request): int
+    {
+        return Request::HEADER_X_FORWARDED_ALL;
+    }
 }
